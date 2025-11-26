ca93dd9cfdec36074431bf6274cbe656065b3ce9
Vaibhav Singhal <vaibhavsinghal0123@gmail.com>
2025-11-27 00:41:26 +0530

    Add files via upload

---
+++ AQ4a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1 = "Hello Sir";
+    string s2 = "Vaibhav here";
+    string s3 = s1 + s2;
+    cout << s3 << endl;
+    return 0;
+}
