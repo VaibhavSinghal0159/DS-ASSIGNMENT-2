1d99001583fb75ae0556a35624385ebfddeabe26
Vaibhav Singhal <vaibhavsinghal0123@gmail.com>
2025-11-27 00:42:55 +0530

    Add files via upload

---
+++ AQ5a.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DiagonalMatrix
+{
+private:
+    int *arr;
+    int n;
+
+public:
+    DiagonalMatrix(int size)
+    {
+        n = size;
+        arr = new int[n];
+        for (int i = 0; i < n; ++i)
+            arr[i] = 0;
+    }
+
+    ~DiagonalMatrix()
+    {
+        delete[] arr;
+    }
+
+    
+    void set(int i, int j, int value)
+    {
+        if (i == j && i >= 0 && i < n)
+            arr[i] = value;
+        
+    }
+
+
+    int get(int i, int j) const
+    {
+        if (i == j && i >= 0 && i < n)
+            return arr[i];
+        else
+            return 0;
+    }
+
+   
+    void display() const
+    {
+        cout << "\nMatrix is:\n";
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (i == j)
+                    cout << arr[i] << " ";
+                else
+                    cout << "0 ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter matrix dimension: ";
+    cin >> n;
+    DiagonalMatrix mat(n);
+
+    cout << "Enter the diagonal elements:\n";
+    for (int i = 0; i < n; ++i)
+    {
+        int val;
+        cout << "Element (" << i << "," << i << "): ";
+        cin >> val;
+        mat.set(i, i, val);
+    }
+
+    mat.display();
+
+    return 0;
+}
