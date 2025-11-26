c0af98fba067f2ad277361c8015d6ce8e81e8365
Vaibhav Singhal <vaibhavsinghal0123@gmail.com>
2025-11-27 00:42:33 +0530

    Add files via upload

---
+++ AQ4e.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c = 'A';
+    if (c >= 'A' && c <= 'Z') {
+        c = c + 32;
+    }
+    cout << c << endl;
+    return 0;
+}
